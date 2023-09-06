f040d9768f794f2bb0f3e717d74eae7db4249cd9
Command_String <rsnedeker20@gmail.com>
2023-09-05 22:26:45 -0400

    Added TODO

--- src/Rest/Channels.php
+++ src/Rest/Channels.php
@@ -5,15 +5,55 @@ namespace CommandString\PHPCord\Rest;
 use CommandString\PHPCord\Abstractions\Channels\Channel;
 use CommandString\PHPCord\Rest\Driver\Http;
 use CommandString\PHPCord\Rest\Driver\Request;
+use Discord\Http\Endpoint;
 use React\Promise\PromiseInterface;
 
+/**
+ * TODO: Finish bindings
+ * modify
+ * delete
+ * getMessages
+ * getMessage
+ * createMessage
+ * crosspostMessage
+ * createReaction
+ * deleteOwnReaction
+ * deleteUserReaction
+ * getReactions
+ * deleteAllReactions
+ * deleteAllReactionsForEmoji
+ * editMessage
+ * deleteMessage
+ * bulkDeleteMessages
+ * editChannelPermissions
+ * getChannelInvites
+ * createChannelInvite
+ * deleteChannelPermission
+ * followAnnouncementChannel
+ * triggerTypingIndicator
+ * getPinnedMessage
+ * pinMessage
+ * unpinMessage
+ * groupDmAddRecipient
+ * groupDmRemoveRecipient
+ * startThread
+ * joinTread
+ * addThreadMember
+ * leaveThread
+ * removeThreadMember
+ * getThreadMember
+ * listThreadMembers
+ * listPublicArchivedThreads
+ * listPrivateArchivedThreads
+ * listJoinedPrivateArchivedThreads
+ */
 class Channels extends Http
 {
     public function get(string $channelId): PromiseInterface
     {
         return $this->mapRequest(
             new Request(
-                url: '/users/@me'
+                url: Endpoint::bind(Endpoint::CHANNEL, $channelId),
             ),
             Channel::class
         );
---
+++ src/Rest/Guilds.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace CommandString\PHPCord\Rest;
+
+use CommandString\PHPCord\Abstractions\Guilds\Guild;
+use CommandString\PHPCord\Rest\Driver\Http;
+use CommandString\PHPCord\Rest\Driver\Request;
+use Discord\Http\Endpoint;
+use React\Promise\PromiseInterface;
+
+/**
+ * TODO: Create bindings
+ * getPreview
+ * modify
+ * delete
+ * getChannels
+ * createChannel
+ * modifyChannelPositions
+ * listActiveThreads
+ * searchMembers
+ * addMember
+ * modifyMember
+ * modifyCurrentMember
+ * modifyCurrentNick
+ * addMemberRole
+ * removeMemberRole
+ * removeMember
+ * getBans
+ * getBan
+ * createBan
+ * removeBan
+ * getRoles
+ * createRole
+ * modifyRolePositions
+ * modifyRole
+ * modifyMFALevel
+ * deleteRole
+ * getPruneCount
+ * beginPrune
+ * getVoiceRegions
+ * getInvites
+ * getIntegrations
+ * deleteIntegration
+ * getWidgetSettings
+ * modifyWidget
+ * getWidget
+ * getVanityUrl
+ * getWidgetImage
+ * getWelcomeScreen
+ * modifyWelcomeScreen
+ * getOnboarding
+ * modifyOnboarding
+ * modifyCurrentUserVoiceState
+ * ModifyUserVoiceState
+ */
+class Guilds extends Http
+{
+    public function get(string $guildId, bool $withCounts = false): PromiseInterface
+    {
+        return $this->mapRequest(
+            new Request(
+                url: Endpoint::bind(Endpoint::GUILD, $guildId),
+                query: [
+                    'with_counts' => $withCounts,
+                ],
+            ),
+            class: Guild::class
+        );
+    }
+}
