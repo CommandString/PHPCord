8ad5ac0952388e719bd7a62c95bb1844635159f6
Command_String <rsnedeker20@gmail.com>
2023-09-10 20:09:23 -0400

    Created Emoji Part

---
+++ src/Parts/Guilds/Emoji.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CommandString\PHPCord\Parts\Guilds;
+
+use CommandString\PHPCord\Parts\Users\User;
+use Stringable;
+
+class Emoji implements Stringable
+{
+    public string $id;
+    public string $name;
+    public ?array $roles;
+    public ?User $user;
+    public ?bool $requireColons;
+    public ?bool $managed;
+    public ?bool $animated;
+    public ?bool $available;
+
+    public function __toString(): string
+    {
+        if (!isset($this->id)) {
+            return $this->name ?? '';
+        }
+
+        return '<' . ($this->animated ?? false ? 'a' : '') . ":{$this->name}:{$this->id}>";
+    }
+}
