65a991ab10b49f7dfbbceded0ab0114d5ba75b0b
Command_String <rsnedeker20@gmail.com>
2023-09-10 20:08:54 -0400

    Finished Application part

--- src/Parts/Application.php
+++ src/Parts/Application.php
@@ -2,6 +2,7 @@
 
 namespace CommandString\PHPCord\Parts;
 
+use CommandString\PHPCord\Parts\Guilds\Guild;
 use CommandString\PHPCord\Parts\Users\User;
 use Tnapf\JsonMapper\Attributes\PrimitiveArrayType;
 use Tnapf\JsonMapper\Attributes\PrimitiveType;
@@ -11,16 +12,35 @@ use Tnapf\JsonMapper\Attributes\SnakeToCamelCase;
 class Application
 {
     public string $id;
-    public string $name;
+    public ?string $name;
     public ?string $icon;
-    public string $description;
+    public ?string $description;
 
     #[PrimitiveArrayType('rpcOrigins', PrimitiveType::STRING, true)]
     public ?array $rpcOrigins;
 
-    public bool $botPublic;
-    public bool $botRequireCodeGrant;
+    public ?bool $botPublic;
+    public ?bool $botRequireCodeGrant;
     public ?string $termsOfServiceUrl;
     public ?string $privacyPolicyUrl;
     public ?User $owner;
+    public ?string $verifyKey;
+
+    // TODO: Create Team part
+    public ?array $team;
+    public ?string $guildId;
+    public ?Guild $guild;
+    public ?string $primarySkuId;
+    public ?string $slug;
+    public ?string $coverImage;
+    public int $flags;
+    public ?int $approximateGuildCount;
+
+    #[PrimitiveArrayType('tags', PrimitiveType::STRING, true)]
+    public ?array $tags;
+
+    // TODO: Create InstallParam part
+    public ?array $installParams;
+    public ?string $customInstallUrl;
+    public ?string $roleConnectionsVerificationUrl;
 }
