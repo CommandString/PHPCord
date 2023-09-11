85d6c4f1859fecea0ca4bf8804b604c6d0369651
Command_String <rsnedeker20@gmail.com>
2023-09-10 22:47:56 -0400

    csf

--- src/Parts/Guilds/Member.php
+++ src/Parts/Guilds/Member.php
@@ -17,6 +17,7 @@ class Member
     /** @var string[] */
     #[PrimitiveArrayType('roles', PrimitiveType::STRING)]
     public ?array $roles;
+
     public string $joinedAt;
     public ?string $premiumSince;
     public ?bool $deaf;
