b2f843ab16cb4f3391ca0100d476452c6c5f5329
Command_String <rsnedeker20@gmail.com>
2023-09-11 22:59:29 -0400

    Finished Part

--- src/Parts/Commands/ApplicationCommandOption.php
+++ src/Parts/Commands/ApplicationCommandOption.php
@@ -9,21 +9,21 @@ use Tnapf\JsonMapper\Attributes\SnakeToCamelCase;
 #[SnakeToCamelCase]
 class ApplicationCommandOption
 {
-    public ApplicationCommandType $type;
+    public ApplicationCommandOptionType $type;
     public string $name;
-    public array $nameLocalizations;
+    public ?array $nameLocalizations;
     public string $description;
-    public array $descriptionLocalizations;
-    public bool $required;
+    public ?array $descriptionLocalizations;
+    public ?bool $required;
 
-    #[ObjectArrayType('options', ApplicationCommandOption::class, nullable: true)]
+    #[ObjectArrayType('choices', ApplicationCommandOptionChoice::class, nullable: true)]
     public ?array $choices;
 
     #[ObjectArrayType('channelTypes', ChannelType::class, nullable: true)]
     public ?array $channelTypes;
-    public int $minValue;
-    public int $maxValue;
-    public int $minLength;
-    public int $maxLength;
-    public bool $autocomplete;
+    public ?int $minValue;
+    public ?int $maxValue;
+    public ?int $minLength;
+    public ?int $maxLength;
+    public ?bool $autocomplete;
 }
